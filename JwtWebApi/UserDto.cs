87f67b5a26038e04d3de493f04677e45759c3925
VladW <VIStrelnikov94@gmail.com>
2023-05-08 02:46:25 +0300

    Add Register

--- JwtWebApi/UserDto.cs
+++ JwtWebApi/UserDto.cs
@@ -2,7 +2,7 @@
 {
     public class UserDto
     {
-        public string Username { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
